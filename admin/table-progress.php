3d3e062726f8d214fe657c76c837721ec7fde746
Evi Dianasari <evidnsr@gmail.com>
2023-11-15 15:42:59 +0800

    Fix front end display

--- admin/table-progress.php
+++ admin/table-progress.php
@@ -127,31 +127,37 @@
 
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
-        <li class="nav-item dropdown pe-3">
+      <li class="nav-item dropdown pe-3">
 
-          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <!-- <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle"> -->
-            <span class="d-none d-md-block dropdown-toggle ps-2">tono</span>
-          </a><!-- End Profile Iamge Icon -->
+<a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+  <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+</a><!-- End Profile Iamge Icon -->
 
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-            <li class="dropdown-header">
-              <h6>tono</h6>
-              <span>admin</span>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+<ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+  <li class="dropdown-header">
+    <h6>Kevin Anderson</h6>
+    <span>Web Designer</span>
+  </li>
+  <li>
+    <hr class="dropdown-divider">
+  </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="logout.php">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-              </a>
-            </li>
+  <li>
+    <hr class="dropdown-divider">
+  </li>
 
-          </ul><!-- End Profile Dropdown Items -->
-        </li><!-- End Profile Nav -->
+  <li>
+    <hr class="dropdown-divider">
+  </li>
+  <li>
+    <a class="dropdown-item d-flex align-items-center" href="login.php">
+      <i class="bi bi-box-arrow-right"></i>
+      <span>Sign Out</span>
+    </a>
+  </li>
+
+</ul><!-- End Profile Dropdown Items -->
+</li><!-- End Profile Nav -->
 
       </ul>
     </nav>
